b21f45d05661a5624305aed7421aa058b0c93522
kali <kali@kali>
2024-11-15 15:29:50 +0530

    Started work on NavBar

---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        heading: ['"Playfair Display"', 'serif'], // For headings
+        body: ['Inter', 'sans-serif'],          // For body text
+      },
+    },
+  },
+  plugins: [],
+};
